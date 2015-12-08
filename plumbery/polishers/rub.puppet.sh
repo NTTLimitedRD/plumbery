3f57fc9fa426bc981a697da3b595e32addb7b8f6
Bernard Paques <bernard.paques@gmail.com>
2015-12-09 00:37:26 +0100

    apply some manifest in puppet

--- plumbery/polishers/rub.puppet.sh
+++ plumbery/polishers/rub.puppet.sh
@@ -2,3 +2,11 @@
 
 # install puppet
 apt-get -y update && apt-get -y install puppet
+
+# apply a manifest eventually
+MANIFEST=$1
+
+if [ -f "$MANIFEST" ]
+then
+    puppet apply --modulepath $MANIFEST
+fi
