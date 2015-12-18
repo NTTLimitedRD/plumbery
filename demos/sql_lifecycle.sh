2b2ad894d8d797e5c1c88bed634fa2200ea27853
Bernard Paques <bernard.paques@gmail.com>
2015-12-18 14:05:59 +0100

    pack demonstration files for the sql blueprint

--- demos/sql_lifecycle.sh
+++ demos/sql_lifecycle.sh
@@ -3,17 +3,17 @@
 # demonstrate the full lifecycle for sql blueprint, across multiple facilities
 
 # now - create all missing parts for the 'sql' fittings
-python build_sql_blueprint.py
+python sql_build.py
 
 # immediately after - start all nodes of 'sql'
-python start_sql_nodes.py
+python sql_start.py
 
 # polish all nodes in 'sql' blueprint
-python polish_sql_blueprint.py
+python sql_polish.py
 
 # time to recycle resources - stop all nodes of 'sql', and reduce the bill
-python stop_sql_nodes.py
+python sql_stop.py
 
 # stop the bill - destroy all nodes of 'sql', and strip related storage
-python destroy_sql_nodes.py
+python sql_destroy.py
 
