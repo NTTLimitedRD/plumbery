7f2fe7f0dd21193ed168439c9b3468f4b31103b5
Bernard Paques <bernard.paques@gmail.com>
2016-02-25 22:42:47 +0100

    manage virtual disks (quantity, type, size)

--- plumbery/polishers/spit.py
+++ plumbery/polishers/spit.py
@@ -98,6 +98,158 @@ class SpitPolisher(PlumberyPolisher):
 
         container._build_balancer()
 
+    def change_node_disk_size(self, node, id, size):
+        """
+        Changes an existing virtual disk
+
+        :param node: the node to be polished
+        :type node: :class:`libcloud.compute.base.Node`
+
+        :param id: the disk unique identifier, as reported by the API
+        :type id: ``str``
+
+        :param size: the disk size, expressed in Giga bytes
+        :type size: ``int``
+
+        """
+
+        while True:
+            try:
+                self.region.ex_change_storage_size(
+                    node=node,
+                    disk_id=id,
+                    size=size)
+
+            except Exception as feedback:
+                if 'RESOURCE_BUSY' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                if 'Please try again later' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                logging.info("- unable to change disk size to {}GB"
+                             .format(size))
+                logging.error(str(feedback))
+
+            break
+
+    def change_node_disk_speed(self, node, id, speed):
+        """
+        Changes an existing virtual disk
+
+        :param node: the node to be polished
+        :type node: :class:`libcloud.compute.base.Node`
+
+        :param id: the disk unique identifier, as reported by the API
+        :type id: ``str``
+
+        :param speed: storage type, either 'standard', 'highperformance' or 'economy'
+        :type speed: ``str``
+
+        """
+
+        while True:
+            try:
+                self.region.ex_change_storage_speed(
+                    node=node,
+                    disk_id=id,
+                    speed=speed)
+
+            except Exception as feedback:
+                if 'RESOURCE_BUSY' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                if 'Please try again later' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                logging.info("- unable to change disk to '{}'"
+                             .format(speed))
+                logging.error(str(feedback))
+
+            break
+
+    def set_node_disk(self, node, id, size, speed='standard'):
+        """
+        Sets a virtual disk
+
+        :param node: the node to be polished
+        :type node: :class:`libcloud.compute.base.Node`
+
+        :param id: the disk id, starting at 0 and growing
+        :type id: ``int``
+
+        :param size: the disk size, expressed in Giga bytes
+        :type size: ``int``
+
+        :param speed: storage type, either 'standard', 'highperformance' or 'economy'
+        :type speed: ``str``
+
+        """
+
+        if size < 1:
+            logging.info("- minimum disk size is 1 GB")
+            return
+
+        if size > 1000:
+            logging.info("- disk size cannot exceed 1000 GB")
+            return
+
+        speed = speed.upper()
+        if speed not in ['STANDARD', 'HIGHPERFORMANCE', 'ECONOMY']:
+            logging.info("- disk speed should be either 'standard' "
+                         "or 'highperformance' or 'economy'")
+            return
+
+        if 'disks' in node.extra:
+            for disk in node.extra['disks']:
+                if disk['scsiId'] == id:
+                    changed = False
+
+                    if disk['size'] != size:
+                        logging.info("- changing disk {} to {}GB".format(
+                            id, size))
+                        self.change_node_disk_size(node, disk['id'], size)
+                        changed = True
+
+                    if disk['speed'] != speed:
+                        logging.info("- changing disk {} to '{}'".format(
+                            id, speed))
+                        self.change_node_disk_speed(node, disk['id'], speed)
+                        changed = True
+
+                    if not changed:
+                        logging.info("- no change in disk {}".format(id))
+
+                    return
+
+        while True:
+            try:
+                logging.info("- adding disk for {}GB '{}'".format(
+                    size, speed))
+                self.region.ex_add_storage_to_node(
+                    node=node,
+                    amount=size,
+                    speed=speed)
+
+            except Exception as feedback:
+                if 'RESOURCE_BUSY' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                if 'Please try again later' in str(feedback):
+                    time.sleep(10)
+                    continue
+
+                logging.info("- unable to add disk {}GB '{}'"
+                             .format(size, speed))
+                logging.error(str(feedback))
+
+            break
+
     def shine_node(self, node, settings, container):
         """
         Finalizes setup of one node
@@ -120,40 +272,22 @@ class SpitPolisher(PlumberyPolisher):
 
         if 'disks' in settings:
             for item in settings['disks']:
+                logging.debug("- setting disk {}".format(item))
                 attributes = item.split()
                 if len(attributes) < 2:
-                    size = int(attributes[0])
-                    speed = 'STANDARD'
+                    logging.info("- malformed disk atributes;"
+                                 " provide disk id and size in GB, e.g., 1 50;"
+                                 " add disk type if needed, e.g., highperformance")
+                elif len(attributes) < 3:
+                    id = int(attributes[0])
+                    size = int(attributes[1])
+                    speed = 'standard'
                 else:
-                    size = int(attributes[0])
-                    speed = attributes[1].upper()
+                    id = int(attributes[0])
+                    size = int(attributes[1])
+                    speed = attributes[2]
 
-                if size < 1 or size > 1000:
-                    logging.info("- disk size cannot exceed 1000")
-                elif speed not in ['STANDARD', 'HIGHPERFORMANCE', 'ECONOMIC']:
-                    logging.info("- disk speed should be 'standard' "
-                                 "or 'highperformance' or 'economic'")
-                else:
-                    while True:
-                        try:
-                            logging.info("- adding disk for {}GB '{}'".format(
-                                size, speed))
-                            self.region.ex_add_storage_to_node(
-                                node,
-                                amount=size,
-                                speed=speed)
-
-                        except Exception as feedback:
-                            if 'RESOURCE_BUSY' in str(feedback):
-                                time.sleep(10)
-                                continue
-
-                            else:
-                                logging.info("- unable to add disk {}GB '{}'"
-                                             .format(size, speed))
-                                logging.error(str(feedback))
-
-                        break
+                self.set_node_disk(node, id, size, speed)
 
         if 'monitoring' in settings:
             self.nodes._start_monitoring(node, settings['monitoring'])
