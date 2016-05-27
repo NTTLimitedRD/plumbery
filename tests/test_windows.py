b2ab3efefe205ce89a4e06b5e250cf9d4d2714ea
anthony-shaw <anthonyshaw@apache.org>
2016-05-27 17:16:42 +1000

    remove windows test

--- tests/test_windows.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from plumbery.polishers.windows import WindowsPolisher
-
-
-class WindowsPolisherTests(unittest.TestCase):
-    def setUp(self):
-        self.polisher = WindowsPolisher()
