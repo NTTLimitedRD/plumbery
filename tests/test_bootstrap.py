ae9f19054c2d462399137440f1c38dfdef56b3af
anthony-shaw <anthonyshaw@apache.org>
2016-06-07 14:14:59 +1000

    added tests for the bootstrap module

---
+++ tests/test_bootstrap.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+from tempfile import gettempdir
+tempdir = gettempdir()
+from mock import MagicMock, patch
+import requests_mock
+import plumbery.bootstrap as b
+
+
+class BootstrapTests(unittest.TestCase):
+    def test_url_cwd(self):
+        url = 'http://test.com/fittings.yaml'
+        args = b.parse_args([url])
+        b.download_file = MagicMock(return_value=True)
+        b.main(args)
+        b.download_file.assert_called_with(url, os.getcwd())
+
+    def test_url_target_directory(self):
+        url = 'http://test.com/fittings.yaml'
+        args = b.parse_args([url, '-o', '/tmp'])
+        b.download_file = MagicMock(return_value=True)
+        b.main(args)
+        b.download_file.assert_called_with(url, '/tmp')
+
+    def test_url_manifest(self):
+        url = 'http://test.com/manifest.mf'
+        args = b.parse_args([url, '-o', '/tmp'])
+        b.download_manifest = MagicMock(return_value=True)
+        b.main(args)
+        b.download_manifest.assert_called_with(url, '/tmp')
+
+    def test_download_manifest(self):
+        url = 'http://test.com/manifest.mf'
+        b.download_file = MagicMock(return_value='test.file')
+        open_name = 'plumbery.bootstrap.open'
+        with patch(open_name, create=True) as mock_open:
+            mock_open.return_value = MagicMock(return_value='test.file')
+            b.download_manifest(url, os.getcwd())
+        b.download_file.assert_called_with(url, os.getcwd())
+
+    def test_download_file(self):
+        with requests_mock.mock() as m:
+            m.get('http://test.com/manifest.mf', text='testing')
+            b.download_file('http://test.com/manifest.mf', tempdir)
+            with open(os.path.join(tempdir, 'manifest.mf')) as tmp_file:
+                self.assertEqual(tmp_file.read(), 'testing')
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
