32cd5a53b7ff952082acd2cacee67796dddbe640
anthony-shaw <anthonyshaw@apache.org>
2016-06-10 16:55:27 +1000

    bump vet

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@
 ###############
 
 FROM python:2.7
-ENV VERSION="0.9.8"
+ENV VERSION="0.9.9"
 ENV MCP_USERNAME=""
 ENV MCP_PASSWORD=""
 ENV SHARED_SECRET=""
--- plumbery/__init__.py
+++ plumbery/__init__.py
@@ -17,4 +17,4 @@
 Are you looking for a cloud plumber? We hope this one will be useful to you
 """
 __all__ = ['__version__']
-__version__ = '0.9.8'
+__version__ = '0.9.9'
